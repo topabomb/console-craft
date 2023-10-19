b547f0074f90a69c4e9659a09aa69464882bf285
topabomb <hualei_hb@163.com>
2023-10-19 15:44:25 +0800

    update 1.0.5

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,19 +1,36 @@
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import { cleandir } from "rollup-plugin-cleandir";
 import json from "@rollup/plugin-json";
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/index.esm.js",
-      format: "es",
-    },
-    {
-      file: "dist/index.umd.js",
-      format: "umd",
-      name: "index.umd.js",
-    },
-  ],
-  plugins: [cleandir("./dist"), commonjs(), typescript(), json()],
-};
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.umd.cjs",
+        format: "umd",
+        name: "index.umd.cjs",
+      },
+    ],
+    plugins: [
+      nodeResolve({
+        extensions: [".ts", ".mjs", ".js", ".json", ".node"],
+        exportConditions: ["node"],
+        preferBuiltins: false,
+      }),
+      commonjs(),
+      typescript(),
+      json(),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.esm.mjs",
+        format: "es",
+      },
+    ],
+    plugins: [commonjs(), typescript(), json()],
+  },
+];
